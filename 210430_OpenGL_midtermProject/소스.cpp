acc121c3ae3bdff0b9514b10c77aa6be574cd57d
ghdus819 <ghdus819@gmail.com>
2021-05-03 13:43:45 +0900

    Add Epicycloid Animation

--- 210430_OpenGL_midtermProject/소스.cpp
+++ 210430_OpenGL_midtermProject/소스.cpp
@@ -9,6 +9,7 @@ using namespace std;
 
 void init();
 void display();
+void idleDisplay();
 void resize(int w, int h);
 void mouseButton(int button, int state, int x, int y);
 void mouseDrag(int x, int y);
@@ -18,7 +19,7 @@ double radian(double angle);
 
 Mesh meshCat, meshDeer, meshLove;
 GLfloat mouseX, mouseY, scaleTotal = 1, scaleX = 1, scaleY = 1, scaleXZ = 1, scaleYZ = 1;
-int angleTotal = 0, anglePart = 0, angleMesh = 0, angleStick = 0;
+int angleTotal = 0, anglePart = 0, angleMesh = 0, angleStick = 0, angleAnim = 0;
 int vecTotalX = 0, vecTotalY = 0, vecTotalZ = 0;
 int vecLeftY = 0, vecCenterX = 0, vecRightZ = 0;
 int vecMesh = 0, vecStick = 0;
@@ -43,6 +44,7 @@ int main(int argc, char** argv) {
 	glutKeyboardFunc(keyboard);
 	glutSpecialFunc(dirKeyboard);
 	glutDisplayFunc(display);
+	glutIdleFunc(idleDisplay);
 	glutMainLoop();
 
 	return 0;
@@ -376,6 +378,327 @@ void display() {
 	}
 
 	
+	glPopMatrix();	// 1-1-1
+
+	glFlush();
+	glutSwapBuffers();
+}
+
+void idleDisplay() {
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glPushMatrix();	// 1
+
+	glRotatef(angleMesh, 0, vecMesh, 0);
+	glTranslatef(-40, -10, 0);
+	meshCat.RenderMesh(VECTOR3D(0.4, 0.4, 0.4), 0.04);
+
+	glTranslatef(0, 15, 0);
+	glPushMatrix();	// 1
+	glPushMatrix();	// 1
+
+	glScalef(0.7, 0.7, 0.7);
+	glTranslatef(-10, 10, 0);
+
+	{
+		double epiCycloidLargeR = 5;
+		double epiCycloidK = 4;
+		double epiCycloidSmallR = epiCycloidLargeR / epiCycloidK;
+
+		glTranslatef(-epiCycloidLargeR, 0, 0);
+
+		glTranslatef(epiCycloidSmallR * ((epiCycloidK + 1) * cos(radian(angleAnim)) - cos((epiCycloidK + 1) * radian(angleAnim))),
+			epiCycloidSmallR * ((epiCycloidK + 1) * sin(radian(angleAnim)) - sin(radian((epiCycloidK + 1) * angleAnim))), 0);
+
+		angleAnim += 4;
+	}
+
+	meshLove.RenderMesh(VECTOR3D(1, 0, 0), 0.1);
+
+	glPopMatrix();	// 1
+	glRotatef(angleMesh, 0, vecMesh, 0);
+	meshLove.RenderMesh(VECTOR3D(1, 0, 0), 0.1);
+
+	glPopMatrix();	// 1
+	//glTranslatef(0, 5, 0);
+	//glRotatef(-angleMesh, vecMesh, 0, 0);
+	//glTranslatef(0, -20, 0);
+	glTranslatef(0, -15, 0);
+	glTranslatef(80, 0, 0);
+	glRotatef(180, 0, 1, 0);
+	meshDeer.RenderMesh(VECTOR3D(0.8, 0.4, 0.2), 0.02);
+	glPopMatrix();	// 1
+	{
+		// Red: x(-), Magenta: x(+) // Green: y(-), Yellow: y(+)// Blue: z(-), Cyan: z(+) //
+		glBegin(GL_LINES);
+		glColor3f(1, 0, 0);
+		glVertex3f(-40, 0, 0);
+		glVertex3f(40, 0, 0);
+
+		glColor3f(0, 1, 0);
+		glVertex3f(0, -40, 0);
+		glVertex3f(0, 40, 0);
+
+		glColor3f(0, 0, 1);
+		glVertex3f(0, 0, -40);
+		glVertex3f(0, 0, 40);
+
+		glColor3f(0, 1, 1);
+		glVertex3f(20, 0, -60);
+		glVertex3f(20, 0, 60);
+
+		glColor3f(1, 0, 1);
+		glVertex3f(-60, -10, 0);
+		glVertex3f(60, -10, 0);
+		glEnd();
+		// Red: x(-), Magenta: x(+) // Green: y(-), Yellow: y(+)// Blue: z(-), Cyan: z(+) //
+	}
+
+	glPushMatrix();	// 1-1 연
+	glPushMatrix();	// 1-2 호
+	glPushMatrix();	// 1-3 유
+
+	glPopMatrix();	// 1-3 유
+
+
+	glTranslatef(42, 0, 0);
+	glRotatef(angleMesh, 0, 0, vecMesh);
+	glTranslatef(-42, 0, 0);
+	glRotatef(-angleMesh, 0, 0, vecMesh);
+
+	{	// 유 - ㅇ
+		glTranslatef(posLeftX, 0, 0);
+		glPushMatrix();	// 1-3 유
+		glPushMatrix();	// 1-3 유
+
+		{
+			glColor3f(1, 0, 1);
+			glTranslatef(-24, 4, 0);
+			glTranslatef(0.5, 2, 0);
+			glRotatef(angleStick, 0, 0, vecStick);
+			glTranslatef(0.5, 2, 0);
+			glPushMatrix();	// 1-3 유
+
+			glRotatef(30, 0, 0, 1);
+			glScalef(1, 4, 1);
+			glutSolidCube(1.0);
+
+			glPopMatrix();	// 1-3 유
+
+			glTranslatef(-1, 1.5, 0);
+			glRotatef(angleStick, 0, 0, vecStick);
+			glRotatef(20, 0, 0, 1);
+			glTranslatef(0, 1.5, 0);
+			glScalef(1, 3, 1);
+
+			glColor3f(1, 1, 0);
+			glutSolidCube(1.0);
+			glColor3f(0, 0, 0);
+			glutWireCube(1.0);
+		}
+
+		glPopMatrix();	// 1-3 유
+
+		{
+			glColor3f(1, 0, 1);
+			glTranslatef(-18, 4, 0);
+			glTranslatef(0.5, 2, 0);
+			glRotatef(-angleStick, 0, 0, vecStick);
+			glTranslatef(0.5, 2, 0);
+			glPushMatrix();	// 1-3 유
+
+			glRotatef(-30, 0, 0, 1);
+			glScalef(1, 4, 1);
+			glutSolidCube(1.0);
+
+			glPopMatrix();	// 1-3 유
+
+			glTranslatef(1, 1.5, 0);
+			glRotatef(-angleStick, 0, 0, vecStick);
+			glRotatef(-20, 0, 0, 1);
+			glTranslatef(0, 1.5, 0);
+			glScalef(1, 3, 1);
+
+			glColor3f(1, 1, 0);
+			glutSolidCube(1.0);
+			glColor3f(0, 0, 0);
+			glutWireCube(1.0);
+		}
+
+
+
+
+		glPopMatrix();	// 1-3 유
+
+
+		glTranslatef(-20, 4, 0);
+		glColor3f(1, 1, 1);
+		glutSolidSphere(4, 30, 30);
+		glColor3f(0, 0, 0);
+		glutWireSphere(4, 30, 30);
+	}
+
+	{	// 유 - ㅠ - ㅡ
+		glTranslatef(0, -5.5, 0);
+		glRotatef(anglePart, 0, vecLeftY, 0);
+		glScalef(15, 2, 2);
+		glScalef(scaleX, 1, 1);
+		glColor3f(1, 1, 1);
+		glutSolidCube(1.0);
+		glColor3f(0, 0, 0);
+		glutWireCube(1.0);
+	}
+
+	glScalef((1.0 / 15), (1.0 / 2), (1.0 / 2));
+
+	{	// 유 - ㅠ - |
+		glTranslatef(-2.5, -4.5, 0);
+		glScalef(2, 7, 2);
+		glColor3f(1, 1, 1);
+		glutSolidCube(1.0);
+		glColor3f(0, 0, 0);
+		glutWireCube(1.0);
+	}
+
+	{	// 유 - ㅠ - |
+		glTranslatef(2.5, 0, 0);
+		glColor3f(1, 1, 1);
+		glutSolidCube(1.0);
+		glColor3f(0, 0, 0);
+		glutWireCube(1.0);
+	}
+
+	glPopMatrix();	// 1-2 호
+	{	// 호 - ㅎ - ㅇ
+		glTranslatef(0, posCenterY, 0);
+		glTranslatef(0, -1, 0);
+		glScalef(1, scaleY, 1);
+		glColor3f(1, 1, 1);
+		glutSolidCone(3.5, 5, 30, 30);
+		glColor3f(0, 0, 0);
+		glutWireCone(3.5, 5, 30, 30);
+	}
+
+	glScalef(1, 1.0 / scaleY, 1);
+
+	glPushMatrix();	// 1-2-1 ㅎ
+
+	glScalef(1, scaleY, 1);
+	{	// 호 - ㅎ - -
+		glColor3f(1, 1, 1);
+		glTranslatef(-0.3, 7, 0);
+		glScalef(4.5, 1.5, 2);
+		glutSolidTetrahedron();
+		glColor3f(0, 0, 0);
+		glutWireTetrahedron();
+	}
+
+	glScalef(1 / 4.5, 1 / 1.5, 1.0 / 2);
+
+	{	// 호 - ㅎ - ㅡ
+		glTranslatef(0, -2, 0);
+		glScalef(10, 1.5, 2);
+		glColor3f(1, 1, 1);
+		glutSolidCube(1.0);
+		glColor3f(0, 0, 0);
+		glutWireCube(1.0);
+	}
+
+	glPopMatrix();	// 1-2-1 ㅎ 
+	{	// 호 - ㅗ - | 
+		glTranslatef(posCenterPart, 0, posCenterPart);
+		glTranslatef(0, -5.5 * scaleY, 0);
+		glScalef(2, 3.5, 2);
+		glRotatef(anglePart, vecCenterX, 0, 0);
+		glColor3f(1, 1, 1);
+		glutSolidCube(1.0);
+		glColor3f(0, 0, 0);
+		glutWireCube(1.0);
+	}
+
+	glScalef(1.0 / 2, 1 / 3.5, 1.0 / 2);
+
+	{	// 호 - ㅗ - ㅡ
+		glTranslatef(0, -2, 0);
+		glScalef(14, 2, 2);
+		glColor3f(1, 1, 1);
+		glutSolidCube(1.0);
+		glColor3f(0, 0, 0);
+		glutWireCube(1.0);
+	}
+
+	glPopMatrix();	// 1-1
+	glPushMatrix();	// 1-1-1
+
+	glTranslatef(0, -10, posRightZ);
+	glRotatef(angleMesh, vecMesh, 0, 0);
+	glTranslatef(0, 10, posRightZ);
+
+	{	// 연 - ㅇ
+		glTranslatef(0, 0, posRightZ);
+		glTranslatef(18, 2.5, 0);
+		glColor3f(1, 1, 1);
+		glutSolidTorus(1, 2.5, 30, 30);
+		glColor3f(0, 0, 0);
+		glutWireTorus(1, 2.5, 30, 30);
+	}
+
+	glPushMatrix();	// 1-1-1
+
+	{	// 연 - ㅕ - -
+		glTranslatef(4.5, 1.5 * scaleYZ, 0);
+		glScalef(4, 2, 1.5);
+		glScalef(1, scaleYZ, scaleYZ);
+		glColor3f(1, 1, 1);
+		glutSolidCube(1.0);
+		glColor3f(0, 0, 0);
+		glutWireCube(1.0);
+	}
+
+	{	// 연 - ㅕ - -
+		glTranslatef(0, -1.5, 0);
+		glScalef(1, 1, 1);
+		glColor3f(1, 1, 1);
+		glutSolidCube(1.0);
+		glColor3f(0, 0, 0);
+		glutWireCube(1.0);
+	}
+
+	glScalef(1.0 / 4, 1.0 / 2, 1.0 / 1.5);
+
+	{	// 연 - ㅕ - |
+		glTranslatef(2, 0.5, 0);
+		glScalef(2, 11, 2);
+		glScalef(1, scaleYZ, scaleYZ);
+		glColor3f(1, 1, 1);
+		glutSolidCube(1.0);
+		glColor3f(0, 0, 0);
+		glutWireCube(1.0);
+	}
+
+	glPopMatrix();	// 1-1-1
+	{	// 연 - ㄴ - |
+		glTranslatef(0, -9.5, 0);
+		glScalef(2, 5, 2);
+		glScalef(scaleXZ, 1, scaleXZ);
+		glRotatef(anglePart, 0, 0, vecRightZ);
+		glColor3f(1, 1, 1);
+		glutSolidCube(1.0);
+		glColor3f(0, 0, 0);
+		glutWireCube(1.0);
+	}
+
+	glScalef(1.0 / 2, 1.0 / 5, 1.0 / 2);
+
+	{	// 연 - ㄴ - -
+		glTranslatef(4, -1.5, 0);
+		glScalef(8, 2, 2);
+		glColor3f(1, 1, 1);
+		glutSolidCube(1.0);
+		glColor3f(0, 0, 0);
+		glutWireCube(1.0);
+	}
+
+
 	glPopMatrix();	// 1-1-1
 
 	glFlush();
